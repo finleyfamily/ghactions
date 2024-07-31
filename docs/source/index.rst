35550f0b0e4d6be3f6f3dae6e243c9e6311d9b79
Kyle Finley <kyle@finley.sh>
2024-07-31 13:15:50 -0400

    setup project

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+#########
+ghactions
+#########
+
+A toolkit for developing GitHub Actions.
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Documentation
+
+  apidocs/index
+
+
+##################
+Indices and Tables
+##################
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+"""Pytest configuration, fixtures, and plugins."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+TEST_DIR = Path(__file__).parent
+
+
+@pytest.fixture(scope="session")
+def fixture_dir() -> Path:
+    """Return path to the ``tests/fixtures/`` directory."""
+    return TEST_DIR / "fixtures"
+
+
+@pytest.fixture(scope="session")
+def root_dir() -> Path:
+    """Return path to the root directory."""
+    return TEST_DIR.parent
